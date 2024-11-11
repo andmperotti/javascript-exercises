9f8076a64639883fb81d0fe0624125247b0f0f85
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 08:58:05 -0800

    Add logic  that extends this functions abilities
    The first iteration only removed the first argument, however with some
    changes we can remove in theory any number of arguments by using the
    arguments object and creating from it an array and filtering those
    values.
    We had also previously set up multiple optional arguments by using
    default values of null, we can continue that however many times we need
    to.
    I think an easier way to do this would involve the spread operator
    instead of continually typing 'removeNine' for example but I'm sure
    there will be times to test that later.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,7 +3,7 @@
 
 const removeFromArray = function(inputArr, removeOne, removeTwo=null, removeThree=null) {
     //removes the non array arguments from that array, and return the array with the remaining elements
-    return inputArr.filter(e=>e!==removeOne)
+    return inputArr.filter(e=>!Array.from(arguments).slice(1).includes(e))
 };
 
 
