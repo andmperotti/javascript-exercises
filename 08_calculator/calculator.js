40b36bf0cd2467145e8d655a22c4abc63a3b5805
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 11:30:24 -0800

    Structure pseudo accordingly, add logic
    We moved the pseudo comments above each pre defined function.
    We added parameters and logic to those functions to achieve desired
    output.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,27 @@
 //by looking at the tests I can see what they want to be done, using the readme I was a little confused that they wanted a calculator that when one function had been run it wanted it to remember that functions return and use it with the next function ran until the sum function was invoked (relating it to a = on a normal physical calculator). 
-//However by looking at and evaluationg the spec file I can see that the add function is just given 2 integer arguments and returns their sum. The subtract function is given two integer arguments and returns the difference between them. The multiply function takes in an array of numbers and multiplies them together. The power function takes in two arguments and retuns the value of the first arg raised to the power of the second args value. And finally the factorial function is like the multiply function but we're given one argument and thus have to build an array of values from 1 up to and including that argument value and then multiply them together.
-const add = function() {
-	
+//However by looking at and evaluating the spec file I can see that the add function is just given 2 integer arguments and returns their sum. 
+const add = function(firstInt, secondInt) {
+  return firstInt+secondInt
 };
-
-const subtract = function() {
-	
+//The subtract function is given two integer arguments and returns the difference between them. The multiply function takes in an array of numbers and multiplies them together.
+const subtract = function(firstInt, secondInt) {
+  return firstInt-secondInt
 };
-
-const sum = function() {
-	
+//the sum function is given an array of integers and returns the sum of those integers
+const sum = function(intArr) {
+  return intArr.reduce((acc,ind)=>acc+ind,0)
 };
-
-const multiply = function() {
-
+//The multiply function takes in an array of numbers and multiplies them together.
+const multiply = function(intArr) {
+  return intArr.reduce((acc,ind)=>acc*ind)
 };
-
-const power = function() {
-	
+//The power function takes in two arguments and returns the value of the first arg raised to the power of the second args value.
+const power = function(baseInt, powerInt) {
+	return Math.pow(baseInt,powerInt)
 };
-
-const factorial = function() {
-	
+//the factorial function is like the multiply function but we're given one argument and thus have to build an array of values from 1 up to and including that argument value and then multiply them together.
+const factorial = function(userInt) {
+	return Array.from({length: userInt}, (e,i)=>i+1).reduce((acc,ind)=>acc*ind)
 };
 
 // Do not edit below this line
