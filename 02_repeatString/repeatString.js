847a3d10f08efe74a6530a6c94e91dcefb8cf982
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 08:35:23 -0800

    Add logic to function
    We had to add parameters for the string input and the integer which
    represented the number of times the user wanted their input string to
    repeat.
    We also had to a for loop that would build through concatenation the
    return string.
    And we included a condition that returned an error message when their
    input integer was less than 0, because we cannot repeat a negative
    number of times.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, numRepeat) {
+    if(numRepeat<0){return 'ERROR'}
+    let resultString = ''
+    for(let i = 1; i<=numRepeat; i++){
+        resultString+=inputString
+    }
+    return resultString
 };
 
 // Do not edit below this line
