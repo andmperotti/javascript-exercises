1e3cab98a2e05cc95002d4dbd0f288f3774b0c35
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 11:23:48 -0800

    Add pseudo code to describe program and functions
    I added a small deciphering of the readme for this exercise and how I
    realized what they wanted.
    I added a brief explanation of what each function needs to do/return.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,3 +1,5 @@
+//by looking at the tests I can see what they want to be done, using the readme I was a little confused that they wanted a calculator that when one function had been run it wanted it to remember that functions return and use it with the next function ran until the sum function was invoked (relating it to a = on a normal physical calculator). 
+//However by looking at and evaluationg the spec file I can see that the add function is just given 2 integer arguments and returns their sum. The subtract function is given two integer arguments and returns the difference between them. The multiply function takes in an array of numbers and multiplies them together. The power function takes in two arguments and retuns the value of the first arg raised to the power of the second args value. And finally the factorial function is like the multiply function but we're given one argument and thus have to build an array of values from 1 up to and including that argument value and then multiply them together.
 const add = function() {
 	
 };
