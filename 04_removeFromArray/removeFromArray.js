6530d5ee5295b68ce0ae66c59f49ee6012f51ee6
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 08:55:36 -0800

    Add logic for removeFromArray function
    This function will take in an array and a value as arguments and it will
     return a new array with that second argument removed from the first
     argument which is the array if that argument exists inside that array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+//create a function
+    //takes in an array, and other arguments 
+
+const removeFromArray = function(inputArr, removeOne, removeTwo=null, removeThree=null) {
+    //removes the non array arguments from that array, and return the array with the remaining elements
+    return inputArr.filter(e=>e!==removeOne)
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
