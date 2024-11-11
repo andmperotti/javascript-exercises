a988c5d1b36405f900f4074e558ceefa6ca3e6a4
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 13:06:38 -0800

    Add logic to accept integers
    Simply added a possible expression to the conditional that allows
    numbers to be make it through the filter method.
    Also as I noted earlier that I would change the lowercase conversion if
    testing for uppercase was done, I have decided not to, because it
     shouldn't matter, if a person input 'Poop' then just want to know if
     they'll get 'poop' back, not 'PooP' and get back 'PooP'.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,7 +2,7 @@
   //returns true if that string is the same as that string in reverse, without factoring word breaks and punctuation 
 //so we'll have to remove the word breaks and punctuation from the argument and then compare it to a reversed version, and we might as well convert its elements to lowercase.
 const palindromes = function (inputString) {
-    let reducedInput = Array.from(inputString.toLowerCase()).filter(e=>e.charCodeAt()>96&&e.charCodeAt()<123).join('')
+    let reducedInput = Array.from(inputString.toLowerCase()).filter(e=>e.charCodeAt()>96&&e.charCodeAt()<123||typeof e ==='number').join('')
     let reversedReducedInput = Array.from(reducedInput).reverse().join('')
     return reducedInput === reversedReducedInput
   };
