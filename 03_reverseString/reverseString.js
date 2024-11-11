7946a3e52d1c77dc003814a2e9b06c06bb4a3fb5
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 08:40:13 -0800

    Add functional logic and parameter
    This functions job is to take in a string and to reverse it.
    I got a little too deep with this function and instead of reversing the
    entire string argument I reversed the words in the string and then each
    letter of each word, built it back into a string and returned it.
    
    And that was way too much work because it was a simple reverse the
    argument string, so I learned that I need to understand the problem a
    little bit better and think about simpler approaches. Oh well.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,25 @@
-const reverseString = function() {
+const reverseString = function (userString) {
+    //add parameter to function
+    //break down user input string into array of words
+    let userWordsArray = userString.split(' ')
+    //loop over the words from that array, and break each word into an array of letters
+    for(let i = 0; i<userWordsArray.length; i++){
+      let currentWord = userWordsArray[i]
+      let wordArray = currentWord.split('')
+      //reverse the order of letters of each word
+      wordArray.reverse()
+      //join them back into a string, and replace the word in the user word array
+      currentWord = wordArray.join('')
+      userWordsArray[i]=currentWord
+    }
+    //reverse the order of words in the array
+    userWordsArray.reverse()
+    //join the array back into a string
+    //return the string back to the user
+    return userWordsArray.join(' ')
+  };
 
-};
+  //This is way too much code, I could have just split the string and reversed it, but instead I made it about individual words ... oops.
 
 // Do not edit below this line
 module.exports = reverseString;
