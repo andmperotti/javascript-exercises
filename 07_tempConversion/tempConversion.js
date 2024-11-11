b1d6352238ff78cb2334e3bac29683e362f2f4bf
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 10:53:10 -0800

    Add functional code
    In the readme the the desired result mentions one decimal place to be
    used trailing the integer if the calculation resulted in a floating
    point number, it did however not say that it was against having .0
    trailing thus this code here is incorrect.
    I could use a conditional however that isn't what they used to get their
      desired result, they multiplied the result by ten then rounded with
    Math.round and then divided by ten to give them a one decimal floating
    point value. I'm just noting this here to save myself if anyone wonders
    why I changed my code from using the toFixed method to using their x10
    then round then divide by ten methodology to meet the desired solution.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahreheitDegree) {
   //to get celsius given fahrenheit: (fahrenheit-32) * (5/9)
-
+  return ((celsiusDegree-32)*(5/9)).toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusDegree) {
   //to get fahrenheit give celsius(celsius * (9/5)) +32
-
+  return (fahreheitDegree*(9/5)+32).toFixed(1)
 };
 
 // Do not edit below this line
