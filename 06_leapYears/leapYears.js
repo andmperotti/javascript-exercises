b87dbaaf12e4323c85333fe048e5b2dd8725eee7
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 10:21:43 -0800

    Add detailed pseudo code for leap year calculation
    I defaulted back to writing detailed pseudo code for the conditional
    steps of the calculation, which helped me to not only decipher the order
     but also how to write the code.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
 const leapYears = function(userYear) {
     //leap years are divisible by 4, but not 100 unless are divisible by 400
+    //thus
+    // if the year is divisible by 4 
+    //   and if divisible by 100 and also by divisible 400
+    //     return true
+    //   otherwise if divisible by 100 and not divisible by 400
+    //     return false
+    //   else (if only divisible by 4, and not by 100 or 400)
+    //     return true
+    // else if not divisible by 4
+        //return false 
 
 };
 
