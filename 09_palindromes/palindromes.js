5ebfbd47bcf28d16647d00ec9563a1f1f17b604b
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 12:58:29 -0800

    Add pseudo code to describe how we'll find answer
    This allows us to focus on the solution and not on the code.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,7 @@
-const palindromes = function () {
+//create a function that takes a string as an argument
+  //returns true if that string is the same as that string in reverse, without factoring word breaks and punctuation 
+//so we'll have to remove the word breaks and punctuation from the argument and then compare it to a reversed version, and we might as well convert its elements to lowercase.
+const palindromes = function (inputString) {
 
 };
 
