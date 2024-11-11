89369f0182414b257d5e5e047dae857502998a3d
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 09:55:49 -0800

    Refactor conditionals together
    I combined the conditionals that return true together so that as long as
    none of them are true then our function runs as planned, otherwise our
    function returns 'ERROR'.
    This resulted in a cleaner to look at function than what it perviously
    was made up of.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,25 +1,22 @@
 //create a function, that takes two integer arguments
 const sumAll = function(startingInt, endingInt) {
     //return the sum of every integer between and including those integer arguments
-    if(startingInt<0||endingInt<0){
-        return 'ERROR'
-    }
-    if(Math.floor(startingInt)!==startingInt||Math.floor(endingInt)!==endingInt){
-        return 'ERROR'
-    }
-    if(typeof startingInt !== 'number' || typeof endingInt !== 'number'){
-        return 'ERROR'
-    }
-    let lowerArg= null
-    let higherArg = null
-    if(startingInt<endingInt){
-      lowerArg=startingInt
-      higherArg=endingInt
+    if((startingInt>=0 && typeof startingInt==='number' && Math.floor(startingInt)===startingInt)
+        && (endingInt>=0 && typeof endingInt==='number' && Math.floor(endingInt)===endingInt)
+    ){
+        let lowerArg= null
+        let higherArg = null
+        if(startingInt<endingInt){
+        lowerArg=startingInt
+        higherArg=endingInt
+        }else{
+        lowerArg=endingInt
+        higherArg=startingInt
+        }
+        return Array.from({length:higherArg-lowerArg+1}, (e,i)=>lowerArg+i).reduce((acc,ele)=>acc+ele,0)
     }else{
-      lowerArg=endingInt
-      higherArg=startingInt
+        return "ERROR"
     }
-    return Array.from({length:higherArg-lowerArg+1}, (e,i)=>lowerArg+i).reduce((acc,ele)=>acc+ele,0)
 };
 
 // Do not edit below this line
