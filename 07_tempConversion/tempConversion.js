0d6e72d75c2bf33c13de3af7a3f0c64c346f4f8e
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 10:45:08 -0800

    Add pseudo code describing calculations
    I added pseudo code that describe the calculations of solving for
    fahrenheit given celsius, and for celsius give fahrenheit.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
 const convertToCelsius = function() {
+  //to get celsius given fahrenheit: (fahrenheit-32) * (5/9)
+
 };
 
 const convertToFahrenheit = function() {
+  //to get fahrenheit give celsius(celsius * (9/5)) +32
+
 };
 
 // Do not edit below this line
