c562af255f0ebdc1b89762f62335489cd5cf988f
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 09:13:23 -0800

    Add functional code that will suffice requirements
    This function needs to take in two integer arguments and return a sum of
     the range of values and including both values itself.
     To do this we create an array of that range, and then use the reduce
     method that returns a sum of that newly built array of integer values.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,7 @@
 //create a function, that takes two integer arguments
-const sumAll = function() {
-  //return the sum of every interger between and including those integer arguments
-
+const sumAll = function(startingInt, endingInt) {
+    //return the sum of every integer between and including those integer arguments
+    return Array.from({length: endingInt-startingInt+1}, (e,i)=>startingInt+i).reduce((acc,ele)=>acc+ele,0)
 };
 
 // Do not edit below this line
