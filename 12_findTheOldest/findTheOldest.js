1eaeee9476889948bbc9e76651f4bd3a1449292b
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 18:09:00 -0800

    Add logic and parameter
    I created a few temp variables to save values as we loop over the
    elements in the argument array of objects, which will allow the function
     to find who is the oldest person and what their name is as that is
    what will be returned.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
-};
-
-// Do not edit below this line
-module.exports = findTheOldest;
+const findTheOldest = function(people) {
+    let currentOldest = null
+    let currentOldestYears = null
+    for(let i = 0; i<people.length; i++){
+      let yearsLived = people[i].yearOfDeath-people[i].yearOfBirth
+      if(yearsLived>currentOldestYears){
+        currentOldest = people[i].name
+        currentOldestYears = yearsLived
+      }
+    }
+    return currentOldest
+  };
