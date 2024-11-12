0de143afab56c55ccef918d18843b3117446f950
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 17:24:48 -0800

    Add parameter and logic
    Added a parameter, which symbolizes the index of an element in the
    sequence to return the value of.
    Added the logic that will build a sequence up to and including the
    argument/desired index element, and returns the appropriate element from
     that built sequence.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(desiredElement) {
+    let sequence = []
+    for(let i = 0; i<desiredElement; i++){
+        if(i===0){
+          sequence.push(1)
+        }else if(i===1){
+          sequence.push(1)
+        }else{
+          sequence.push(sequence[i-2]+sequence[i-1])
+        }
+    }
+    return sequence[desiredElement-1]
 };
 //A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers.
 //return a specific member of the sequence; thus we'll need to build up to that member of the sequence
