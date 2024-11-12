3207b8385c1655ba88924a9f89986536e0e0871e
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 17:52:02 -0800

    Add conditionals and type coercion
    To satisfy tests, I added explicit type coercion, which overwrites the
    parameter with the return of passing the input data into the number
    constructor. I also added a conditional for when the desired element is
    0, which should return 0. I also added a conditional to check whether
    the argument is a negative number.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
 const fibonacci = function(desiredElement) {
+    desiredElement=Number(desiredElement)
+    if(desiredElement===0){
+      return 0
+    }
+    if(desiredElement<0){
+      return 'OOPS'
+    }
+  
     let sequence = []
     for(let i = 0; i<desiredElement; i++){
         if(i===0){
@@ -10,7 +18,7 @@ const fibonacci = function(desiredElement) {
         }
     }
     return sequence[desiredElement-1]
-};
+  };
 //A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers.
 //return a specific member of the sequence; thus we'll need to build up to that member of the sequence
 
