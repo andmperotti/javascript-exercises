15f5e2f966319b0c9fd43e2c33889ac4609eb431
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 16:51:30 -0800

    Change conditional expression to correctly filter
    I figured out an error of mine was that I was trying to use typeof to
    allow numbers through to the resulting arrays, however when the numbers
    were being passed to typeof they were as strings and not numbers, and
    I then realized typeof can't be used how I wanted for anything because
    they'll all be within strings. Oops, well now I've seen that problem,
    and hopefully will not do that again.
    I have switched to using character codes, and have included the codes
    for integers from 0 up to and including 9, so that those will be
    included in the strings.
    I also changed the structure of the filter method, as I believe it was
    passing the outcome of the first condition and not the joint outcome
    of the first and second conditions into an if elif else chain and it now
     works correctly.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,7 +2,15 @@
   //returns true if that string is the same as that string in reverse, without factoring word breaks and punctuation 
 //so we'll have to remove the word breaks and punctuation from the argument and then compare it to a reversed version, and we might as well convert its elements to lowercase.
 const palindromes = function (inputString) {
-    let reducedInput = Array.from(inputString.toLowerCase()).filter(e=>e.charCodeAt()>96&&e.charCodeAt()<123||typeof e ==='number').join('')
+    let reducedInput = Array.from(inputString.toLowerCase())
+      .filter(e=>{
+        if(e.charCodeAt()>96&&e.charCodeAt()<123){
+          return true
+        }else if(e.charCodeAt()>47&&e.charCodeAt()<58){
+          return true
+        }else{
+          return false        }
+      }).join('')
     let reversedReducedInput = Array.from(reducedInput).reverse().join('')
     return reducedInput === reversedReducedInput
   };
