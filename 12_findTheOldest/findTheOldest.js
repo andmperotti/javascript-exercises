74c47360dcee628aff6340e27e2acd83984d322b
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 18:27:09 -0800

    Add logic to account for undefined yearOfDeath
    By using  a new Date() object we can acquire the current year and we
    save it as a variable and if a person in the people array doesn't have
    a yearOfDeath we make it the current year, and so now we can compare
    their age in years up to the current year against other people and infer
     who is the oldest.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,12 @@
 const findTheOldest = function(people) {
-    let currentOldest = null
-    let currentOldestYears = null
+    let currentOldest = null;
+    let currentOldestYears = null;
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear()
     for(let i = 0; i<people.length; i++){
+      if(!people[i].yearOfDeath){
+        people[i].yearOfDeath = currentYear
+      }
       let yearsLived = people[i].yearOfDeath-people[i].yearOfBirth
       if(yearsLived>currentOldestYears){
         currentOldest = people[i]
