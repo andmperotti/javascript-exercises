fc27c4fa926ac64d3b49ef36d77d834000a18d57
Andrew Perotti <Perotti28@gmail.com>
2024-11-11 17:02:50 -0800

    Add pseudo code as a reminder to what fibonacci is
    I can delete this later but for now it will serve as a good reminder so
    I don't have to alt tab or switch between tabs in vs code.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,8 @@
 const fibonacci = function() {
 
 };
+//A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers.
+//return a specific member of the sequence; thus we'll need to build up to that member of the sequence
 
 // Do not edit below this line
 module.exports = fibonacci;
