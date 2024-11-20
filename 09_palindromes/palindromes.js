5c6ab385f0f3b2b85fd4e6ae66d9aa42ad5bd621
Andrew Perotti <Perotti28@gmail.com>
2024-11-20 13:47:35 -0800

    Omit and restart exercises
    I am redoing these as the course wants, here I have written a function
    that checks if a string just made up of alphabetical letters is a
    palindrome.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,19 +1,6 @@
-//create a function that takes a string as an argument
-  //returns true if that string is the same as that string in reverse, without factoring word breaks and punctuation 
-//so we'll have to remove the word breaks and punctuation from the argument and then compare it to a reversed version, and we might as well convert its elements to lowercase.
-const palindromes = function (inputString) {
-    let reducedInput = Array.from(inputString.toLowerCase())
-      .filter(e=>{
-        if(e.charCodeAt()>96&&e.charCodeAt()<123){
-          return true
-        }else if(e.charCodeAt()>47&&e.charCodeAt()<58){
-          return true
-        }else{
-          return false        }
-      }).join('')
-    let reversedReducedInput = Array.from(reducedInput).reverse().join('')
-    return reducedInput === reversedReducedInput
-  };
+function palindromes(str){
+  return str === str.split('').reverse().join('')
+}
 
 // Do not edit below this line
 module.exports = palindromes;
