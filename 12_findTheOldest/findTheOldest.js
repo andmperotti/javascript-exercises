8f6aa4355ed1115887385795acf20b3b4539173e
Andrew Perotti <Perotti28@gmail.com>
2024-11-20 17:03:13 -0800

    Omit existing code, Add new code
    I was told to do this exercise again.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,20 +1,21 @@
 const findTheOldest = function(people) {
-    let currentOldest = null;
-    let currentOldestYears = null;
-    let currentDate = new Date();
-    let currentYear = currentDate.getFullYear()
-    for(let i = 0; i<people.length; i++){
-      if(!people[i].yearOfDeath){
-        people[i].yearOfDeath = currentYear
-      }
-      let yearsLived = people[i].yearOfDeath-people[i].yearOfBirth
-      if(yearsLived>currentOldestYears){
-        currentOldest = people[i]
-        currentOldestYears = yearsLived
-      }
+  let oldestPerson = null
+  let oldestAge = null
+
+
+  for(let i = 0; i<people.length; i++){
+    let currentPerson = people[i]
+    let currentPersonAge = people[i].yearOfDeath-people[i].yearOfBirth
+
+    if(currentPersonAge>oldestAge){
+      oldestPerson=currentPerson
+      oldestAge=currentPersonAge
+
     }
-    return currentOldest
-  };
+  }
+  return oldestPerson
+};
+
   
   // Do not edit below this line
   module.exports = findTheOldest;
