e55f94025683a544ff697d4f04cc35681855484d
Andrew Perotti <Perotti28@gmail.com>
2024-11-20 16:28:32 -0800

    Omit code and try exercise again
    After failing a few times I did look at the solution, and later came
    back to write my own version of it.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,26 +1,13 @@
-const fibonacci = function(desiredElement) {
-    desiredElement=Number(desiredElement)
-    if(desiredElement===0){
-      return 0
-    }
-    if(desiredElement<0){
-      return 'OOPS'
-    }
-  
-    let sequence = []
-    for(let i = 0; i<desiredElement; i++){
-        if(i===0){
-          sequence.push(1)
-        }else if(i===1){
-          sequence.push(1)
-        }else{
-          sequence.push(sequence[i-2]+sequence[i-1])
-        }
-    }
-    return sequence[desiredElement-1]
-  };
-//A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers.
-//return a specific member of the sequence; thus we'll need to build up to that member of the sequence
+function fibonacci(target){
+  target = parseInt(target)
+  if(target===0){return 0}
+  if(target<0){return 'OOPS'}
 
+  let sequence = [1,1]
+  for(let i = 2; i<=target; i++){
+    sequence.push(sequence[i-2]+sequence[i-1])
+  }
+  return sequence[target-1]
+}
 // Do not edit below this line
 module.exports = fibonacci;
